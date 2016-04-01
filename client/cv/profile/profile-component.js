f966a2ba5562889cd27fc1b3bb69b811ea1299b3
Albert Gracia <albert.gracia@ecrome.com>
2016-04-01 10:00:51 +0200

    -finish resolve-loader component -some clean up and some css tweaks

--- client/cv/profile/profile-component.js
+++ client/cv/profile/profile-component.js
@@ -1,43 +1,43 @@
 angular.module('cv').directive('profile', function () {
     return {
-      restrict: 'E',
-      templateUrl: 'client/cv/profile/profile.html',
-      controllerAs: 'main',
-      controller: function ($scope, $reactive) {
-        $reactive(this).attach($scope);
+        restrict: 'E',
+        templateUrl: 'client/cv/profile/profile.html',
+        controllerAs: 'main',
+        controller: function ($scope, $reactive) {
+            $reactive(this).attach($scope);
 
-        var profile = Profiles.findOne({});
-        this.helpers({
-          profile: () => {
-            return profile;
-          }
-        });
+            var profile = Profiles.findOne({});
+            this.helpers({
+                profile: () => {
+                    return profile;
+                }
+            });
 
-        this.getIndexOfSkill = (elem) => {
-          return getIndexOfSkill(elem);
-        };
+            this.getIndexOfSkill = (elem) => {
+                return getIndexOfSkill(elem);
+            };
 
-        this.toDate = (string) => {
-          return toDate(string);
-        };
-        
+            this.toDate = (string) => {
+                return toDate(string);
+            };
 
-        function getIndexOfSkill(elem){
-          var index = -1;
-          if(profile){
-            index = profile.personalInformation.skills.indexOf(elem);
-          }
-          return index;
+
+            function getIndexOfSkill(elem) {
+                var index = -1;
+                if (profile) {
+                    index = profile.personalInformation.skills.indexOf(elem);
+                }
+                return index;
+            }
+
+            function toDate(string) {
+                var date = null;
+                if (profile && string != '') {
+                    var aux = string.split('-');
+                    date = new Date(parseInt(aux[1]), parseInt(aux[0]) - 1);
+                }
+                return date;
+            }
         }
-
-        function toDate(string){
-          var date = null;
-          if(profile && string != ''){
-            var aux = string.split('-');
-            date = new Date(parseInt(aux[1]), parseInt(aux[0])-1);
-          }
-          return date;
-        }
-      }
     }
-  });
+});
--- client/cv/resolveloader/resolve-loader-component.js
+++ client/cv/resolveloader/resolve-loader-component.js
@@ -1,28 +1,17 @@
-angular.module('cv').directive('resolveLoader', function($rootScope, $timeout) {
+angular.module('cv').directive('resolveLoader', function ($rootScope, $timeout) {
 
     return {
         restrict: 'E',
         replace: true,
-        template: '<div class="alert alert-success ng-hide"><strong>Welcome!</strong> Content is loading, please hold.</div>',
-        link: function(scope, element) {
+        templateUrl: 'client/cv/resolveloader/resolveloader.html',
+        link: function (scope, element) {
 
-            $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams, options) {
-                console.log('event',event);
-                console.log('toState',toState);
-                console.log('toParams',toParams);
-                console.log('fromState',fromState);
-                console.log('fromParams',fromParams);
-                console.log('options',options);
-
+            $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams, options) {
                 if (fromState.name !== "") return;
                 element.removeClass('ng-hide');
-                /*$timeout(function() {
-                    console.log('hide??');
-                    element.removeClass('ng-hide');
-                });*/
             });
 
-            $rootScope.$on('$stateChangeSuccess', function() {
+            $rootScope.$on('$stateChangeSuccess', function () {
                 element.addClass('ng-hide');
             });
         }
